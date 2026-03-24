5b83454291a5c7f6137c440b5680fee045e79925
glebski36 <45667135+glebski36@users.noreply.github.com>
2026-03-24 08:46:58 +0200

    Create CTEwValues.sql

---
+++ patterns/data/CTEwValues.sql
@@ -0,0 +1,18 @@
+/* VALUES construct */
+WITH cte1 (col1, col2, col3) AS
+(
+SELECT * 
+FROM
+	(VALUES ('ABC', 'BCD', 'CDE')
+         ,('DEF', 'EFG', 'FGH')
+  	                            ) AS A (SomeColumn1, SomeColumn2, SomeColumn3)
+)
+, cte2 (colA, colB) AS
+(
+  SELECT val1 AS c1, vol2 AS c2
+  UNION ALL
+  SELECT val3, val4
+) 
+SELECT * 
+  FROM cte1
+;
